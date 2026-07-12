b28ae1ac26cd0b843e656703de5b2968aaf2e6df
Mauricio Rivera <mauriver21@gmail.com>
2026-07-12 18:42:10 -0500

    add exception handler

--- src/controllers/user.py
+++ src/controllers/user.py
@@ -1,33 +1,19 @@
-from fastapi import HTTPException
-
 from interfaces.UserCreate import UserCreate
 from interfaces.UserUpdate import UserUpdate
 import repositories.user as user_repository
 
 
 def list(page: int = 0, page_size: int = 10):
-    try:
-        return user_repository.list(page, page_size)
-    except Exception as error:
-        raise HTTPException(status_code=500, detail=str(error)) from error
+    return user_repository.list(page, page_size)
 
 
 def create(user: UserCreate):
-    try:
-        return user_repository.create(user)
-    except Exception as error:
-        raise HTTPException(status_code=500, detail=str(error)) from error
+    return user_repository.create(user)
 
 
 def update(id: str, user: UserUpdate):
-    try:
-        return user_repository.update(id, user)
-    except Exception as error:
-        raise HTTPException(status_code=500, detail=str(error)) from error
+    return user_repository.update(id, user)
 
 
 def remove(id: str):
-    try:
-        return user_repository.logical_delete(id)
-    except Exception as error:
-        raise HTTPException(status_code=500, detail=str(error)) from error
+    return user_repository.logical_delete(id)
