56c0e4f980da7a8035254ccef73fdf02ab1e08b6
Mauricio Rivera <mauriver21@gmail.com>
2026-07-12 18:38:30 -0500

    WIP error handling

--- src/controllers/user.py
+++ src/controllers/user.py
@@ -1,19 +1,33 @@
+from fastapi import HTTPException
+
 from interfaces.UserCreate import UserCreate
 from interfaces.UserUpdate import UserUpdate
 import repositories.user as user_repository
 
 
 def list(page: int = 0, page_size: int = 10):
-    return user_repository.list(page, page_size)
+    try:
+        return user_repository.list(page, page_size)
+    except Exception as error:
+        raise HTTPException(status_code=500, detail=str(error)) from error
 
 
 def create(user: UserCreate):
-    return user_repository.create(user)
+    try:
+        return user_repository.create(user)
+    except Exception as error:
+        raise HTTPException(status_code=500, detail=str(error)) from error
 
 
 def update(id: str, user: UserUpdate):
-    return user_repository.update(id, user)
+    try:
+        return user_repository.update(id, user)
+    except Exception as error:
+        raise HTTPException(status_code=500, detail=str(error)) from error
 
 
 def remove(id: str):
-    return user_repository.logical_delete(id)
+    try:
+        return user_repository.logical_delete(id)
+    except Exception as error:
+        raise HTTPException(status_code=500, detail=str(error)) from error
