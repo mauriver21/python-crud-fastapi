83a5cd423cfbea25a767e65d654ba56693d60f06
Mauricio Rivera <mauriver21@gmail.com>
2026-07-12 19:24:34 -0500

    stable user crud

--- src/routes.py
+++ src/routes.py
@@ -5,5 +5,5 @@ router = APIRouter()
 
 router.add_api_route("/users", user_controller.list, methods=["GET"])
 router.add_api_route("/users", user_controller.create, methods=["POST"])
-router.add_api_route("/users/:id", user_controller.update, methods=["PUT"])
-router.add_api_route("/users/:id", user_controller.remove, methods=["DELETE"])
+router.add_api_route("/users/{id}", user_controller.update, methods=["PUT"])
+router.add_api_route("/users/{id}", user_controller.remove, methods=["DELETE"])
